25e83b49b8a1627ca43937c9edbe1860c92cf1c9
admin <faizulkhan56@gmail.com>
2024-05-21 21:41:23 +0600

    get req

--- nodejs-poridhi/node-js/class-two-be-get-request/app.js
+++ nodejs-poridhi/node-js/class-two-be-get-request/app.js
@@ -10,6 +10,48 @@ res.send('student information')
 
 })
 
+app.get('/employee',(req,res)=>{
+
+    const id = req.query.id;
+    const dept = req.query.dept;
+
+    console.log(id);
+    console.log(dept);
+
+    const result =  {
+        name: "Faizul",
+        age: 38,
+        salary:3500
+        }
+
+    if(id){
+
+    res.send(result)
+
+    }
+
+    else
+
+    {
+    res.send('no id')
+    } })
+
+
+    app.get('/employee/manager',(req,res)=>{
+
+        res.send('i am manger')
+        
+        
+        })
+   app.get('/employee/:id/:dept',(req,res)=>{
+            const id = req.params.id
+            const dept = req.params.dept
+            console.log(id)
+            console.log(dept)
+            res.send(`I am manager with ${id} path parameter ${dept}`);
+            
+            
+            })
 
 
 app.listen(4040,() =>{
