9e9bb6d617c49ed20c190c6328de9d881c171410
faizul.khan <faizul.khan@dngroup.com>
2024-05-26 09:54:25 +0600

    route update delete added

--- nodejs-poridhi/node-js/class-five-orm/src/index.ts
+++ nodejs-poridhi/node-js/class-five-orm/src/index.ts
@@ -1,43 +1,15 @@
 import {escape} from "querystring"
 import "reflect-metadata";
 import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
 import express from "express";
+import { router as studentRouter } from "../src/routes/Student";
 const app = express();
 app.use(express.json());
 const port = 3000;
 
 AppDataSource.initialize().then(async () => {
-   
-    const userRepo = AppDataSource.getRepository(User);
-    app.get('/students',async(req,res)=>{
-     
-        const users=await userRepo.find();
-        res.send(users);
-        })
 
-    app.post('/student',async(req,res)=>{
-
-        const{firstName, lastName, age} = req.body
-        const user =new User()
-        user.lastName = lastName
-        user.firstName = firstName
-        user.age = age
-
-        try{
-
-            const savedUser=await userRepo.save(user)
-            res.status(201).send(savedUser)
-        }
-
-        catch(error){
-
-            res.status(500).send("fail to save")
-        }
-
-
-    })
-
+    app.use(studentRouter)
     app.listen(port, () => {
         console.log(`Server running on http://localhost:${port}`);
     });
