f45673413e4a311414928b84bfd12e3166aafeb7
faizul.khan <faizul.khan@dngroup.com>
2024-06-24 17:24:14 +0600

    delete post route added

--- nodejs-poridhi/node-js/piyush-class-five-REST-api/index.js
+++ nodejs-poridhi/node-js/piyush-class-five-REST-api/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json")
+const fs = require("fs")
 
 const app = express();
 const PORT = 8000;
+
+//Middleware - Plugin
+app.use(express.urlencoded({extended:false}));
 
 //Routes
 app.get('/users', (req, res)=>{
@@ -33,18 +37,32 @@ app.route('/api/users/:id').get((req, res)=>{
 
 })
 
-.post((req, res) =>{
+//.post((req, res) =>{
     // TODO : edit the user with id
-    return res.json({status : 'Pending'})
-})
-.patch((req, res) =>{
+   // return res.json({status : 'Pending'})
+//})
+
+app.post('/api/users' , (req , res)=>{
+    // TODO : Create new user
+    const  body = req.body;
+    //console.log("body",body);
+    users.push({...body , id:users.length+1});
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err) => {
+        if (err) {
+          return res.json({ status: "error", message: err.message });
+        }
+        return res.json({ status: "success", id: users.length });
+      });
+    });
+
+//.patch((req, res) =>{
     // TODO : edit the user with id
-    return res.json({status : 'Pending'})
-})
-.delete((req , res)=>{
-    // TODO : delete the user with id
-    res.json({status : 'Pending'})
-})
+   // return res.json({status : 'Pending'})
+//})
+//.delete((req , res)=>{
+   // // TODO : delete the user with id
+   // res.json({status : 'Pending'})
+//})
 
 
 
@@ -62,6 +80,26 @@ app.route('/api/users/:id').get((req, res)=>{
 //     // TODO : delete the user with id
    //return res.json({status: "pending"});
  //})
+
+
+
+ app.delete('/api/users/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const userIndex = users.findIndex(user => user.id === id);
+  
+    if (userIndex === -1) {
+      return res.status(404).json({ status: "error", message: "User not found" });
+    }
+  
+    users.splice(userIndex, 1);
+  
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err) => {
+      if (err) {
+        return res.status(500).json({ status: "error", message: err.message });
+      }
+      return res.json({ status: "success", message: `User with id ${id} deleted` });
+    });
+  });
 
 app.listen(PORT , ()=>{
     console.log(`Server started at Port ${PORT}`)
